2ac68dbc0823940b87dad6f2e572c5dafaef5518
Aditya Maulana Ardiansyah <mam25037@gmail.com>
2026-06-29 14:03:19 +0700

    Initial commit

---
+++ app/src/main/java/com/arkliener/financeku/DatabaseHelper.java
@@ -0,0 +1,12 @@
+package com.arkliener.financeku;
+
+public class DatabaseHelper {
+    private static final String DATABASE_NAME = "financeku.db";
+    private static final int DATABASE_VERSION = 1;
+    public static final String TABLE_TRANSAKSI = "transaksi";
+    public static final String COL_ID = "id";
+    public static final String COL_JUDUL = "judul";
+    public static final String COL_NOMINAL = "nominal";
+    public static final String COL_JENIS = "jenis";
+    public static final String COL_TANGGAL = "tanggal";
+}
